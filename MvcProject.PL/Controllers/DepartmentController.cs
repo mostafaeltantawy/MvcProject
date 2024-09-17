a2768b01bbd27b0f24cc434da4da6017ea502bcc
mostafaeltantawy <eltantawymostafa2@gmail.com>
2024-09-17 18:30:32 +0300

    Department PL

--- MvcProject.PL/Controllers/DepartmentController.cs
+++ MvcProject.PL/Controllers/DepartmentController.cs
@@ -2,6 +2,7 @@
 using MvcProject.BLL.Interfaces;
 using MvcProject.BLL.Repositories;
 using MvcProject.DAL.Models;
+using System;
 using System.Linq;
 
 namespace MvcProject.PL.Controllers
@@ -40,5 +41,88 @@ namespace MvcProject.PL.Controllers
 
             return View(department);
         }
+
+        // BaseURl/Department/Details/100
+        public IActionResult Details(int? id , string ViewName = "Details") 
+        {
+            if(id == null)
+                return BadRequest(); // Stauts code 400
+
+            var department = _departmentsRepo.Get(id.Value); 
+            if(department == null)
+                return NotFound();
+            return View(ViewName , department);
+
+
+        }
+
+
+        public IActionResult Edit(int? id) 
+        {
+            //if (id == null)
+            //{
+            //    return BadRequest();    
+            //}
+            //var department = _departmentsRepo.Get(id.Value);
+            //if(department == null)
+            //{
+            //    return NotFound();
+            //}
+            //return View(department);
+            return Details( id , "Edit");
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Department department ,[FromRoute] int id) 
+        {
+            if (id != department.Id)
+                return BadRequest(); 
+            if (ModelState.IsValid) 
+            {
+                try
+                {
+                    _departmentsRepo.Update(department);
+                    return RedirectToAction(nameof(Index));
+                }
+                catch (Exception ex)
+                {
+                    ModelState.AddModelError(string.Empty, ex.Message);
+                    
+                }
+           
+            }
+            return View(department); 
+
+        }
+
+
+        public IActionResult Delete(int? id) 
+        {
+            return Details(id  , "Delete"); 
+
+        }
+
+        [HttpPost]
+        public IActionResult Delete(Department department , [FromRoute]int id) 
+        {
+            if(department.Id != id)
+                return BadRequest();
+
+            try
+            {
+				_departmentsRepo.Delete(department);
+				return RedirectToAction(nameof(Index));
+
+
+			}
+			catch (Exception ex)
+            {
+
+                ModelState.AddModelError(string.Empty , ex.Message);
+                return View(department);
+            }
+        }
+
     }
 }
